962c64361830a281d4afd8672bdb5139d33eb006
estherpinos <estherpinos1@gmail.com>
2023-10-25 14:47:45 +0200

    Add [html, echo]

--- gestioneForm.php
+++ gestioneForm.php
@@ -7,3 +7,17 @@ $lunghezza_paragrafo= strlen($paragrafo);
 
 $paragrafo_modificato = str_replace($parola,'***',$paragrafo);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> <?php echo "Paragrafo: $paragrafo" ?> </p>
+    <p> <?php echo "lunghezza paragrafo: $lunghezza_paragrafo" ?> </p>
+    <p> <?php echo "Paragrafo modificato: $paragrafo_modificato" ?> </p>
+</body>
+</html>
